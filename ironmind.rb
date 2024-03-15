a3f69afffb3dd66b30d57ea9ef5f85677b90c9f4
Jaden Arceneaux <arceneauxjaden@gmail.com>
2024-03-14 18:23:06 -0700

    updated to latest release

--- ironmind.rb
+++ ironmind.rb
@@ -1,8 +1,8 @@
 class Ironmind < Formula
   desc "A BrainF*ck interpreter written in Rust"
   homepage "https://crates.io/crates/ironmind/"
-  url "https://github.com/Jadens-arc/Ironmind/releases/download/v0.3.15/ironmind.tar.gz"
-  sha256 "9a8bfa2d9dec6c9c941bf34156c3d3dd427c5f50153e387666f142f259777fc3"
+  url "https://github.com/Jadens-arc/Ironmind/releases/download/v0.3.16/ironmind.tar.gz"
+  sha256 "80bc92c508b1fa6b49e4b43cfb62e9c51599d1261a906620b5824566db75c406"
   license "MIT"
 
   def install
